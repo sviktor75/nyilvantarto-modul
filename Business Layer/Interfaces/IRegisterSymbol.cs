28c5337d874df606f9423694f4a6b1b04f229c8d
janoserdody <erdody.janos@gmail.com>
2019-03-26 00:36:03 +0100

    interfaces update

---
+++ Business Layer/Interfaces/IRegisterSymbol.cs
@@ -0,0 +1,20 @@
+﻿using Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Layer.Interfaces
+{
+    public interface IRegisterSymbol
+    {
+        ISymbol this[int ID] { get; set; }
+
+        IError Add(ISymbol symbol);
+
+        IError Remove(ISymbol symbol);
+
+        IError RemoveByID(ISymbol symbol);
+    }
+}
